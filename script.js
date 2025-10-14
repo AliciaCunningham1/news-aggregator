6eb92ac18a705b1e7e05692747b30b3b8450d9b3
AliciaCunningham1 <e2945464@student.dcccd.edu>
2025-10-14 14:17:10 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,86 @@
+const ConfigManager = (function() {
+    let instance;
+
+    function createInstance() {
+        return {
+            theme: 'dark',
+            apiUrl: 'https://newsapi.org/v2/top-headlines',
+            apiKey: 'e0a72113ba364e46883d653ca5d5ac58'
+        };
+    }
+
+    function getInstance() {
+        if (!instance) instance = createInstance();
+        return instance;
+    }
+
+    return {
+        getInstance: getInstance
+    };
+})();
+
+const NewsFetcher = (function () {
+    const config = ConfigManager.getInstance();
+
+    async function fetchArticles() {
+        try {
+            const response = await fetch(`${config.apiUrl}?country=us&apiKey=${config.apiKey}`);
+            const data = await response.json();
+            return data.articles || [];
+        } catch (error) {
+            console.log('Error fetching articles:', error);
+            return [];
+        }
+    }
+
+    return {
+        getArticles: fetchArticles
+    };
+})();
+
+function NewsFeed() {
+    this.observers = [];
+    this.articles = [];
+}
+
+NewsFeed.prototype = {
+    subscribe: function(observerFn) {
+        this.observers.push(observerFn);
+    },
+    unsubscribe: function(observerFn) {
+        this.observers = this.observers.filter(fn => fn !== observerFn);
+    },
+    notify: function(article) {
+        this.observers.forEach(fn => fn(article));
+    },
+    addArticle: function(article) {
+        this.articles.push(article);
+        this.notify(article);
+    }
+};
+
+const newsFeed = new NewsFeed();
+
+function updateHeadline(article) {
+    const headlineElement = document.getElementById('headline').querySelector('p');
+    headlineElement.textContent = article.title;
+}
+
+function updateArticleList(article) {
+    const articleListElement = document.getElementById('articles');
+    const listItem = document.createElement('li');
+    listItem.textContent = article.title;
+    articleListElement.appendChild(listItem);
+}
+
+newsFeed.subscribe(updateHeadline);
+newsFeed.subscribe(updateArticleList);
+
+NewsFetcher.getArticles().then(articles => {
+    articles.forEach(article => {
+        newsFeed.addArticle(article);
+    });
+});
+
+const configInfo = ConfigManager.getInstance();
+document.getElementById('configInfo').textContent = `Theme: ${configInfo.theme}`;
